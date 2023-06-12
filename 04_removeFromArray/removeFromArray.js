a71b40b1b5a68ce43fd46e2484f2b99e18e870b1
Travis Packer <travispacker1533@yahoo.co.uk>
2023-06-13 00:25:02 +0100

    Finished removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(array=[], ...remove) // Rest operator, puts extra arguments in new array
+{
+    let next = array.filter(b => !remove.includes(b));
 
+    return next;
 };
 
 // Do not edit below this line
