7ed5ea09db3618191bd01dfc53269c994c92e46a
Travis Packer <travispacker1533@yahoo.co.uk>
2023-06-13 01:02:16 +0100

    Completed sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(a, b) 
+{
+    if(isNaN(a) || isNaN(b))
+    {
+        return "ERROR";
+    }
 
+    if(a < 0 || b < 0)
+    {
+        return "ERROR";
+    }
+
+    return (Math.abs(a-b) + 1) * (a + b) / 2;
 };
 
 // Do not edit below this line
