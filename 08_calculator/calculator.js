36404d55174d834a690dbf1c85d2bddf1ec2dca3
Travis Packer <travispacker1533@yahoo.co.uk>
2023-06-15 22:12:24 +0100

    Finished calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function(num1, num2) 
+{
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) 
+{
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(nums=[]) 
+{
+	return nums.reduce((a, b) => a + b, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(...nums) 
+{
+  return nums.reduce((a, b) => a * b);
 };
 
-const power = function() {
-	
+const power = function(num, power) 
+{
+	return Math.pow(num, power);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) 
+{
+  if(num === 0 || num === 1)
+  {
+    return 1;
+  }
+  else if(num < 0)
+  {
+    console.log("ERROR");
+  }
+	else
+  {
+    for(let i = num - 1; i >= 1; i--)
+    {
+      num *= i;
+    }
+
+    return num;
+  }
 };
 
 // Do not edit below this line
