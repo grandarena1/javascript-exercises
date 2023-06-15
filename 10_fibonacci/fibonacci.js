d6801a06a2d6ca8b7564995f610827e36ab7c751
Travis Packer <travispacker1533@yahoo.co.uk>
2023-06-15 23:12:31 +0100

    Finished fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) 
+{
+    if(num < 0)
+    {
+        return "OOPS";
+    }
 
+    const fibPart = [0, 1];
+
+    for(let index = 1; index < num; index++)
+    {
+        fibPart.push(fibPart[index] + fibPart[index - 1]);
+    }
+
+    return fibPart[num];
 };
 
 // Do not edit below this line
