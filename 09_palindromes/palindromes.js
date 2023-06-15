c8cf750d9303e6114b6bfc88267410477413fd2b
Travis Packer <travispacker1533@yahoo.co.uk>
2023-06-15 22:55:10 +0100

    Finished palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function(word) 
+{
+    const regex = /[^a-z0-9]/g;
+    const reLow = word.toLowerCase().replace(regex, '');
+    return reLow.split('').reverse().join('') === reLow;
 };
 
 // Do not edit below this line
